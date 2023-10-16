1b1d388a7c64721ec2873c7f1c6955cac1c424fb
houssam980 <houssam.ghmari1243@gmail.com>
2023-10-16 09:34:15 -0700

    envm file

---
+++ _gt_envm.c
@@ -0,0 +1,27 @@
+#include "_shell.h"
+
+/**
+*_get_envm - get envirment
+*@valr: variable
+*Return: return renvmd if excists otherwise NULL
+*/
+
+char *_get_envm(char *valr)
+{
+int iterat;
+char *temp, *valu, *envmd, *kygen;
+for(iterat = 0; environ[iterat]; iterat++)
+{
+temp = _strdup(environ[iterat]);
+kygen = strtok(temp, "=");
+if (_strcmp(kygen, valr) == 0)
+{
+valu = strtok(NULL, "\n"); 
+envmd = _strdup(valu);/*declaring the duplucat of valu before freeing the memory*/
+free(temp);
+return (envmd);
+}
+free(temp), temp = NULL;/*free memory*/
+}
+return (NULL);
+}
