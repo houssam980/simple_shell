a2cef283fa19914cee2e2d76da88c4cc7cd090d2
houssam980 <houssam.ghmari1243@gmail.com>
2023-10-16 10:11:58 -0700

    main

---
+++ main_prg.c
@@ -0,0 +1,35 @@
+#include "_shell.h"
+
+/**
+* main - main programme executer
+* @ac: amount of argum
+* @av: argums
+* Return: return 0
+*/
+
+int main (int ac, char **argv)
+{
+char *ln = NULL; /*line*/
+int stus = 0, id_x = 0;   /*status*/ /*id_x*/
+char **command_lns = NULL; /*command*/
+(void)ac;
+while (1)
+{
+ln = red_line(); /*read line func*/
+if (ln == NULL) /*Ctrl D to exit*/
+{
+if (isatty(STDIN_FILENO)) /*if standar input is a terminal */
+write(STDOUT_FILENO, "\n", 1);
+return (stus);
+}
+id_x++;
+command_lns = tokeniser(ln);
+if (!command_lns)
+continue;  /*display the prompt again if no command submited*/
+
+if (is_builtin(command_lns[0]))
+built_in(command_lns, argv, &stus, id_x);
+else
+stus = _excute(command_lns, argv, id_x);
+}
+}
