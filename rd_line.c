5f3bce260ef2134ce62d9af350a9dcfa0b2641ac
houssam980 <houssam.ghmari1243@gmail.com>
2023-10-16 10:20:22 -0700

    line

---
+++ rd_line.c
@@ -0,0 +1,23 @@
+#include "_shell.h"
+ 
+/**
+*red_line - read command line
+*@void: void
+*Return : return line if exists
+*/
+
+char *red_line(void)
+{
+char *ln = NULL ; /*line*/
+ssize_t nuv;/*new line*/
+size_t lenght = 0;/*lenght*/
+if (isatty(STDIN_FILENO)) /*if standar input is a terminal */
+write(STDOUT_FILENO, "$ ", 2);  /*Display to the user the prompt sign withe space and handel 2 chars*/
+nuv = getline(&ln, &lenght, stdin );
+if (nuv == -1)
+{
+free(ln);
+return (NULL);
+}
+return (ln);
+}
